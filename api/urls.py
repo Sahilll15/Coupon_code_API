c5c6452b98c3334f44fcfb441e8ffc57542c2030
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-07-20 14:35:38 +0530

    added update delete

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getData),
-    path('add/', views.createData),
+    path('', views.getData, name='get-data'),
+    path('<int:pk>/', views.getDataById, name='get-data-by-id'),  
+    path('createdata/', views.createData, name='create-data'),
+    path('updatedata/<int:pk>/', views.updateData, name='update-data'),
+    path('deletedata/<int:pk>/', views.deleteData, name='delete-data'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,8 @@
-from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from base.models import Coupon
 from .serializers import CouponSerializer
-from rest_framework import status
-
 
 @api_view(['GET'])
 def getData(request):
@@ -11,10 +10,43 @@ def getData(request):
     serializer = CouponSerializer(data, context={'request': request}, many=True)
     return Response(serializer.data)
 
+@api_view(['GET'])
+def getDataById(request, pk):
+    try:
+        coupon = Coupon.objects.get(pk=pk)
+    except Coupon.DoesNotExist:
+        return Response({'error': 'Coupon not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CouponSerializer(coupon, context={'request': request})
+    return Response(serializer.data)
 
 @api_view(['POST'])
 def createData(request):
     serializer = CouponSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def updateData(request, pk):
+    try:
+        coupon = Coupon.objects.get(pk=pk)
+    except Coupon.DoesNotExist:
+        return Response({'error': 'Coupon not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CouponSerializer(coupon, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def deleteData(request, pk):
+    try:
+        coupon = Coupon.objects.get(pk=pk)
+    except Coupon.DoesNotExist:
+        return Response({'error': 'Coupon not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    coupon.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
